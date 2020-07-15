1e8e9ee009a5148b2f5ced0f8f0b252c03da8599
Unknown <josh.asi@hotmail.co.nz>
2020-07-16 08:59:16 +1200

    Created initial routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { getVehiclesRequest } from './redux/vehicles/requests';
+// import { getVehicleTypesRequest } from './redux/vehicle-types/requests';
+// import { useDispatch } from 'react-redux';
 
 const App = () => {
-  return <div>Hi</div>;
+  // const dispatch = useDispatch();
+  // React.useEffect(() => {
+  //   dispatch(getVehicleTypesRequest());
+  // }, [dispatch]);
+
+  return (
+    <Provider store={store}>
+      <SideBar />
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/vehicles' component={Vehicles} />
+            <Route path='/locations' component={Locations} />
+            <Route path='/drivers' component={Drivers} />
+            <Route path='/messages' component={Messages} />
+            <Route path='/dashboard' component={Dashboard} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
+
+const SideBar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar__title'>VMS</div>
+    </div>
+  );
+};
+
+const Home = () => <div>Home</div>;
+const Vehicles = () => <div>Vehicles</div>;
+const Locations = () => <div>Locations</div>;
+const Drivers = () => <div>Drivers</div>;
+const Messages = () => <div>Messages</div>;
+const Dashboard = () => <div>Dashboard</div>;
+const PageNotFound = () => <div>Page not found. Click here to go home.</div>;
