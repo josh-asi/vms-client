2ce66fcf540d36f3094164c856a1615fcbf1b919
Unknown <josh.asi@hotmail.co.nz>
2020-07-15 15:08:12 +1200

    Jest and Enzyme setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('should render without error', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
